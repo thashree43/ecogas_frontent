15b9dcb6404bc7b794d52c6e8960cc81d3fa9cef
Thashreef <thashreefkhan4@gmail.com>
2024-11-10 14:06:43 +0530

    hosting

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,3 +1,3 @@
-export const baseurluser = "http://100.26.191.8/api/user"
-export const baseurlagent = "http://100.26.191.8/api/agent"
-export const basseurladmin = "http://100.26.191.8/api/admin"
+export const baseurluser = "https://www.zenvogue.online/api/user"
+export const baseurlagent = "https://www.zenvogue.online/api/agent"
+export const basseurladmin = "https://www.zenvogue.online/api/admin"
