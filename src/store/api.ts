f9c3675c893fef38fb24d5d103443ae291b43016
Thashreef <thashreefkhan4@gmail.com>
2024-11-19 12:09:10 +0530

    neew

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,3 +1,4 @@
 export const baseurluser = `${import.meta.env.VITE_LOCAL_PORT}/api/user`;
 export const baseurlagent = `${import.meta.env.VITE_LOCAL_PORT}/api/agent`;
 export const basseurladmin = `${import.meta.env.VITE_LOCAL_PORT}/api/admin`;
+console.log("the url baseurl",import.meta.env.VITE_LOCAL_PORT)
